e874228a32c6035bbce9f79f2d3f236b9113cc7a
Phil <a@as-macbook-pro.home>
2019-11-30 19:09:34 +0000

    Add global package

---
+++ globalp/python/packtg/helper_python_global.py
@@ -0,0 +1,4 @@
+
+
+sample_warc_loc = "/Users/a/IdeaProjects/The-Spark-Workshop/resources/spark/webcorpus/warc.sample"
+sample_wet_loc = "/Users/a/IdeaProjects/The-Spark-Workshop/resources/spark/webcorpus/wet.sample"
---
+++ globalp/scala/packtg/HelperScalaGlobal.scala
@@ -0,0 +1,7 @@
+package packtg
+
+object HelperScalaGlobal {
+
+
+
+}
