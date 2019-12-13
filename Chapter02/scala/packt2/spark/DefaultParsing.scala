599a7826fb1e95ba43afee6191892752b95279ba
Phil <a@as-macbook-pro.home>
2019-12-13 18:12:31 +0000

    Add code for §2

---
+++ Chapter02/scala/packt2/spark/DefaultParsing.scala
@@ -0,0 +1,25 @@
+package packt2.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import packt2.HelperScala
+
+object DefaultParsing {
+  def main(args: Array[String]) {
+
+    val inputLocWet: String = HelperScala.sampleWetLoc
+    val session: SparkSession = HelperScala.createSession(2, "Default crawl parsing")
+    session.sparkContext.setLogLevel("ERROR")  // avoids printing of info messages
+    val records: RDD[String] = session.sparkContext.textFile(inputLocWet)
+
+    println(records.getNumPartitions)
+
+    records.take(50).foreach(line => {
+      println(line)
+      println("---------------------------")
+    })
+    println("#############################")
+    println(s"Total records: ${records.count()}")
+
+  }
+}
