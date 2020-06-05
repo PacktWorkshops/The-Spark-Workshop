c4e80f3eb60609250273d99349ffd484dfde375a
Landon <landonrobinson92@gmail.com>
2020-06-05 10:16:31 -0600

    Update Exercise11_03.py
    modify to match book

--- Chapter11/Exercise11_03/Exercise11_03.py
+++ Chapter11/Exercise11_03/Exercise11_03.py
@@ -1,15 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark.sql import SQLContext
-
-
-# Function to read data in RDD
-def read(animal): print("I am no pet, for I am a " + animal['category'], animal['name'] + "!")
-
-
-# Function to showcase filtered data
-def read_non_cats(animal): print("I am a " + animal['name'] + ", which does not start with a 'c'.")
-
 
 # Create a Spark Session
 spark = SparkSession\
@@ -19,21 +8,22 @@ spark = SparkSession\
     .getOrCreate()
 
 sc = spark.sparkContext
-sqlContext = SQLContext(sc)
 
 # create a sample set of data as an RDD
 categorized_animals = [("dog", "pet"), ("cat", "pet"), ("bear", "wild"), ("cat", "pet"), ("cat", "pet")]
 animalDataRDD = sc.parallelize(categorized_animals)
-animals = animalDataRDD.map(lambda x: Row(name=x[0], category=x[1]))
-animalsDF = sqlContext.createDataFrame(animals)
+
+# turn that RDD into a DataFrame
+animalsDF = spark.createDataFrame(animalDataRDD, ['name', 'category'])
 
 # Filter out animal names that start with 'c'
 nonCats = animalsDF.filter("name not like 'c%'")
-nonCats.foreach(read_non_cats)
+nonCats.show()
 
 # Filter out animal names that are not 'cat'
 nonCatsTwo = animalsDF.where("name != 'cat'")
+nonCatsTwo.show()
 
 # Filter out animals that are not in the 'pet' category
 nonPets = animalsDF.filter("category != 'pet'")
-nonPets.foreach(read)
+nonPets.show()
