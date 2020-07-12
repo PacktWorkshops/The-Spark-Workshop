19c4761842443cfc084400d89aadab585b426ddc
Phil <a@as-macbook-pro.home>
2020-07-12 11:13:42 +0100

    Rename variable ex 1.2

--- Chapter01/Exercise1_02/Exercise1_02.py
+++ Chapter01/Exercise1_02/Exercise1_02.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
               "untouched", "expanse", "of", "their", "background"]
     session = create_session(2, "Python map closure")
     words_rdd: RDD = session.sparkContext.parallelize(words)  # Creation of Spark data structure
-    wordLengths: RDD = words_rdd.map(lambda word: len(word))  # passing a closure to Spark's `map`
-    print(wordLengths.collect())  # printing result to stdout
+    word_lengths: RDD = words_rdd.map(lambda word: len(word))  # passing a closure to Spark's `map`
+    print(word_lengths.collect())  # printing result to stdout
     # Result: [11, 4, 9, 3, 3, 5, 2, 6, 4, 8, 2, 3, 9, 7, 2, 5, 10] order might differ
