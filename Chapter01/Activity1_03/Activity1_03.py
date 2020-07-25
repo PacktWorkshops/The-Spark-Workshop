9ae047b302efbb11089c001b079ea2ef8413abd7
Phil <a@as-MacBook-Pro.local>
2020-07-25 21:08:26 +0100

    Finish activity

--- Chapter01/Activity1_03/Activity1_03.py
+++ Chapter01/Activity1_03/Activity1_03.py
@@ -4,12 +4,12 @@ from utilities01_py.helper_python import *
 
 if __name__ == "__main__":
     session: SparkSession = create_session(2, "Zipf validation")
-    linesDf: DataFrame = session.read.text(novella_location).withColumnRenamed('value', 'sentences')
+    lines_df: DataFrame = session.read.text(novella_location).withColumnRenamed('value', 'sentences')
 
     # from pyspark.ml.feature import RegexTokenizer
     # tokenizer = RegexTokenizer(inputCol='sentences', outputCol='words', pattern='\W+')
     #
-    # tokenized = tokenizer.transform(linesDf)
+    # tokenized = tokenizer.transform(lines_df)
     # samples = tokenized.take(100)
     # for sample in samples:
     #     print(sample)
